9a7283606253fa9aeff8d719bbf86fbbbceea1fd
Keita Nonaka <iKonnyaku40@yahoo.co.jp>
2019-09-20 09:41:24 -0400

    linting, update npm modules (#2)

--- public/javascripts/effect.js
+++ public/javascripts/effect.js
@@ -1,12 +1,11 @@
-
 function select_checked(target) {
-    select = document.getElementById('edit_priority').options;
-    for(let i = 0; i < select.length; i++ ){
-        if(select[i].value === target){
-            select[i].selected = true;
-            break;
-        }
+  select = document.getElementById('edit_priority').options
+  for (let i = 0; i < select.length; i++) {
+    if (select[i].value === target) {
+      select[i].selected = true
+      break
     }
+  }
 }
 
 // window.onload = function() {
